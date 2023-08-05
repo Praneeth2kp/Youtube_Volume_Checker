c97d672db4b61194291259078ed39f04042c2a09
praneeth2kp <praneeth2kp@gmail.com>
2023-08-06 00:18:33 +0530

    final commit

--- src/youtubesearch.js
+++ src/youtubesearch.js
@@ -5,12 +5,13 @@ import 'react-datepicker/dist/react-datepicker.css';
 import './youtubesearch.css';
 
 function YoutubeVolumeSearch() {
-    const [keyword, setKeyword] = useState('Gojo Satoru');
+    const [keyword, setKeyword] = useState('');
     const [searchResult, setSearchResult] = useState(null);
     const [totalResults, setTotalResults] = useState(0);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [selectedYear, setSelectedYear] = useState(null);
+    const [videoStatsPerMonth, setVideoStatsPerMonth] = useState([]);
 
     const handleChange = (event) => {
         setKeyword(event.target.value);
@@ -27,6 +28,7 @@ function YoutubeVolumeSearch() {
             setLoading(true);
             setError(null);
             setSearchResult(null);
+            setVideoStatsPerMonth([]);
 
             if (!keyword) {
                 setError('Please enter a keyword');
@@ -40,21 +42,29 @@ function YoutubeVolumeSearch() {
                 return;
             }
 
+            const year = selectedYear.getFullYear();
+
             let params = {
                 q: keyword,
                 part: 'snippet',
                 type: 'video',
                 maxResults: 50,
-                key: 'AIzaSyC_rXms59iU06fuQlNrZ8jsrwXooRCp3K0',
+                key: 'AIzaSyCJr0tj--OThVpP7wYX4yLhE3hTrm43Av8',
             };
 
-            if (selectedYear) {
-                const year = selectedYear.getFullYear();
-                const fromDate = `${year}-01-01T00:00:00Z`;
-                const toDate = `${year + 1}-01-01T00:00:00Z`;
-                params.publishedAfter = fromDate;
-                params.publishedBefore = toDate;
+            const videoStats = [];
+            for (let month = 0; month < 12; month++) {
+                const fromDate = new Date(year, month, 1);
+                const toDate = new Date(year, month + 1, 1);
+                params.publishedAfter = fromDate.toISOString();
+                params.publishedBefore = toDate.toISOString();
+                const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+                    params,
+                });
+                videoStats.push({ month: fromDate.toLocaleString('default', { month: 'long' }), count: response.data.pageInfo.totalResults });
             }
+
+            setVideoStatsPerMonth(videoStats);
 
             const response = await axios.get(`https://www.googleapis.com/youtube/v3/search`, {
                 params,
@@ -71,39 +81,51 @@ function YoutubeVolumeSearch() {
 
     return (
         <div>
-            <div className='inputsearch'>
-                <div className='inputs'>
-                    <div className='input-container'>
-                        <input
-                            type="text"
-                            className={`keyword ${keyword ? 'active' : ''}`}
-                            placeholder="Enter keyword"
-                            value={keyword}
-                            onChange={handleChange}
-                        />
-                        <div className='underline'></div>
-                    </div>
-                    <div className='input-container'>
+            <h1 style={{ textAlign: 'center', color: 'White' }}>Youtube Search Volume Application</h1>
+            <div className='group-1'>
+                <div className='inner-group-1'>
+                    <input
+                        type="text"
+                        className="keyword"
+                        placeholder="Enter keyword"
+                        value={keyword}
+                        onChange={handleChange}
+                    />
+                    <div style={{ width: '270px' }}>
                         <DatePicker
-                            className={`calender ${selectedYear ? 'active' : ''}`}
+                            className="calender"
+                            placeholderText='YYYY'
                             selected={selectedYear}
                             onChange={handleYearChange}
                             dateFormat="yyyy"
                             showYearPicker
                             scrollableYearDropdown
                         />
-                        <div className='underline'></div>
                     </div>
                 </div>
-
-                <button onClick={handleSearch} className='search-button'>Search</button>
+                <div>
+                    <button onClick={handleSearch} className="search-button"> Search</button>
+                </div>
             </div>
-            {loading && <p>Loading....</p>}
+
+            {loading && <p>Loading...</p>}
+
             {error && <p>{error}</p>}
+
             {searchResult && (
                 <div>
-                    <h2>Search Results:</h2>
                     <h2>Total Videos Found: {totalResults}</h2>
+                    <h2>Video Uploads per Month:</h2>
+                    <div className='flexed'>
+                        <div className="video-stats">
+                            {videoStatsPerMonth.map((stat) => (
+                                <p key={stat.month}>
+                                    {stat.month}: {stat.count}
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+
                     <div className="flex-container">
                         {searchResult.map((item) => (
                             <div key={item.id.videoId} className="flex-item">
@@ -119,7 +141,6 @@ function YoutubeVolumeSearch() {
                             </div>
                         ))}
                     </div>
-
                 </div>
             )}
         </div>
